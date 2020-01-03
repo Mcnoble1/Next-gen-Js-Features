dfeab4e8d6cee6c88e2f806ea2e17c5f9dcfd77e
Idowu Festus <idowufestustemiloluwa@gmail.com>
2020-01-03 07:34:58 -0800

    spread and rest operator

--- next-gen.js
+++ next-gen.js
@@ -4,34 +4,34 @@
 // myName = "Mcnoble";
 // console.log(myName);
 
-// // let myName = "Noble";
-// // console.log(myName);
-// // myName = "Mcnoble";
-// // console.log(myName);
+ let myName = "Noble";
+ console.log(myName);
+ myName = "Mcnoble";
+ console.log(myName);
 
-// // const myName = "Noble";
-// // console.log(myName);
-// // myName = "Mcnoble";
-// // console.log(myName);
+ const myBrandName = "McNoble";
+ console.log(myBrandName);
+ // myBrandName = "Mcnoble";
+ // console.log(myName);
 
 // // Arrow Functions
-// const printMyName = (name) => {
-// 	console.log(name);
-// }
-// printMyName('Noble');
+const printMyName = (name) => {
+console.log(name);
+ }
+printMyName('Noble');
 
-// const multiply = number => {
-// 	return number * 2;
-// }
-// console.log(multiply(2));
+const multiply = number => {
+	return number * 2;
+ }
+console.log(multiply(2));
 
-// // const multiply = number => number * 2;
-// // multiply(2);
+const mult = number => number * 2;
+ mult(2);
 
-// const details =(name, age) => {
-// 	console.log(name, age);
-// }
-// details('Festus', 20);
+const details =(name, age) => {
+	console.log(name, age);
+ }
+details('Festus', 20);
 
 
 // // Exports and Imports
@@ -53,34 +53,72 @@
 // import {clean} from './utility.js'
 
 // classes
+// class Human {
+// 	constructor() {
+// 		this.gender = "male";
+// 		this.age = 20;
+// 	}
+// 	printMyGender () {
+// 		console.log(this.gender);
+// 	}
+// 	printMyAge () {
+// 		console.log(this.age);
+// 	}
+// }
+
+// class Person extends Human {
+// 	// For variables
+// 	constructor() {
+// 		super();           
+// 		this.name = "Noble";
+// 		this.gender = "female";
+// 	}
+// 	// For methods
+// 	printMyName() {
+// 		console.log(this.name);
+// 	}
+// }
+
+
+// Using ES7
 class Human {
-	constructor() {
-		this.gender = "male";
-		this.age = 20;
-	}
-	printMyGender () {
+	gender = "male";
+	printMyGender = () => {
 		console.log(this.gender);
-	}
-	printMyAge () {
-		console.log(this.age);
 	}
 }
 
 class Person extends Human {
-	// For variables
-	constructor() {
-		super();           
-		this.name = "Noble";
-		this.gender = "female";
-	}
-	// For methods
-	printMyName() {
+	name = "Festus";
+	printMyName = () => {
 		console.log(this.name);
 	}
 }
+
 // Instantiation of the class
 const person = new Person();
 person.printMyName();
 person.printMyGender();
-person.printMyAge();
 
+
+// Spread and Rest Operator ...
+// Spread
+const oldArray = [2,4,6];
+const newArray = [...oldArray,8,10];
+console.log(newArray);
+
+const bioData = {
+	name : "Festus",
+	age : 20,
+	job : "web developer"
+}
+const newBioData = {
+	...bioData,
+	level : 300
+}
+console.log(newBioData);
+// Rest
+const filter = (...args) => {
+	return args.filter(el => (el / 2) === 1);
+} 
+console.log(filter(1,2,3));
