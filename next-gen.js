c7ff062fae5fbd91590926cbc1455999a485b1d0
Idowu Festus <idowufestustemiloluwa@gmail.com>
2020-01-03 06:39:00 -0800

    classes

--- next-gen.js
+++ next-gen.js
@@ -1,53 +1,86 @@
-// Let anad Const
-var myName = "Noble";
-console.log(myName);
-myName = "Mcnoble";
-console.log(myName);
-
-// let myName = "Noble";
+// // Let anad Const
+// var myName = "Noble";
 // console.log(myName);
 // myName = "Mcnoble";
 // console.log(myName);
 
-// const myName = "Noble";
-// console.log(myName);
-// myName = "Mcnoble";
-// console.log(myName);
+// // let myName = "Noble";
+// // console.log(myName);
+// // myName = "Mcnoble";
+// // console.log(myName);
 
-// Arrow Functions
-const printMyName = (name) => {
-	console.log(name);
+// // const myName = "Noble";
+// // console.log(myName);
+// // myName = "Mcnoble";
+// // console.log(myName);
+
+// // Arrow Functions
+// const printMyName = (name) => {
+// 	console.log(name);
+// }
+// printMyName('Noble');
+
+// const multiply = number => {
+// 	return number * 2;
+// }
+// console.log(multiply(2));
+
+// // const multiply = number => number * 2;
+// // multiply(2);
+
+// const details =(name, age) => {
+// 	console.log(name, age);
+// }
+// details('Festus', 20);
+
+
+// // Exports and Imports
+// // person.js
+// const person = {
+// 	name: 'Max';
+// }
+
+// export default person;
+
+// // utility.js
+// export const clean = () => {   }
+// export const baseData = 10;
+
+// // app.js
+// import person from './person.js'
+// import prs from './person.js'
+// import {baseData} from './utility.js'
+// import {clean} from './utility.js'
+
+// classes
+class Human {
+	constructor() {
+		this.gender = "male";
+		this.age = 20;
+	}
+	printMyGender () {
+		console.log(this.gender);
+	}
+	printMyAge () {
+		console.log(this.age);
+	}
 }
-printMyName('Noble');
 
-const multiply = number => {
-	return number * 2;
+class Person extends Human {
+	// For variables
+	constructor() {
+		super();           
+		this.name = "Noble";
+		this.gender = "female";
+	}
+	// For methods
+	printMyName() {
+		console.log(this.name);
+	}
 }
-console.log(multiply(2));
+// Instantiation of the class
+const person = new Person();
+person.printMyName();
+person.printMyGender();
+person.printMyAge();
 
-// const multiply = number => number * 2;
-// multiply(2);
-
-const details =(name, age) => {
-	console.log(name, age);
-}
-details('Festus', 20);
-
-
-// Exports and Imports
-// person.js
-const person = {
-	name: 'Max';
-}
-
-export default person;
-
-// utility.js
-export const clean = () => {   }
-export const baseData = 10;
-
-// app.js
-import person from './person.js'
-import prs from './person.js'
-import {baseData} from './utility.js'
-import {clean} from './utility.js'
